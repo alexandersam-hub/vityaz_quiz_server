b14e5f0c76ff0a769a69f3358cc67366bf818ede
stan1i <alexandersamedov@gmail.com>
2022-06-15 11:35:29 +0300

    add socket.io v10

--- services/TelegramSendService.js
+++ services/TelegramSendService.js
@@ -4,7 +4,7 @@ const client = new WebSocket('ws://127.0.0.1:8101');
 class TelegramSendService{
     sendMessageToAdmin(data){
         try{
-            console.log()
+            console.log('telegramSendAdmin', data)
             client.send(JSON.stringify({action: 'telegramSendAdmin', data}));
             return true
         }catch (e) {
