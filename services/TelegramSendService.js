bd530758c826e7a06a13ac7e989f099615042c1e
stan1i <alexandersamedov@gmail.com>
2022-06-15 11:41:51 +0300

    add path 1.1.2

--- services/TelegramSendService.js
+++ services/TelegramSendService.js
@@ -4,7 +4,7 @@ const client = new WebSocket('ws://127.0.0.1:8101');
 class TelegramSendService{
     sendMessageToAdmin(data){
         try{
-            console.log('telegramSendAdmin', data)
+            console.log('telegramSend', data)
             client.send(JSON.stringify({action: 'telegramSendAdmin', data}));
             return true
         }catch (e) {
