6d26ddc4042ec1209a9416164d60f5ca918f677f
Stan1i <alexandersamedov@gmail.com>
2023-04-26 00:26:53 +0300

    remove token user count

--- services/tokenService.js
+++ services/tokenService.js
@@ -39,28 +39,28 @@ class TokenService{
         // const tokenData = await tokenModel.findOne({user:userId})
         const tokenInfoData = await infoTokenModel.findOne({user:user.id})
         // console.log('user', user)
-        // if(user.role === 'admin'){
+        if(user.role === 'admin'){
             return {warning:false, token}
-        // }
+        }
         if(tokenInfoData){
             //если количество токенов у пользователя превышено
-            if(tokenInfoData.tokens.length >= process.env.MAX_COUNT_TOKEN_BY_USER){
+            // if(tokenInfoData.tokens.length >= process.env.MAX_COUNT_TOKEN_BY_USER){
 
-                let countUpdateMonth = 0
-                tokenInfoData.dateLastUpdate.forEach(t=>{
-                    const currentDate = new Date(Date.now());
-                    currentDate.setMonth(currentDate.getMonth() - 1);
-                    if(currentDate<t)
-                        countUpdateMonth++
-                })
-                if(countUpdateMonth>= process.env.MAX_COUNT_REPLACE_TOKEN)
-                    return {warning:true, message:`Количество авторазий превысело максимально возможное: ${ process.env.MAX_COUNT_TOKEN_BY_USER}`}
-                else
-                    return {warning:true, user:user.id, tokenInfo:{countUpdateMonth, lastCountUpdates:process.env.MAX_COUNT_TOKEN_BY_USER - countUpdateMonth, tokens:tokenInfoData.tokens,}, message:`Количество авторазий превысело максимально возможное: ${ process.env.MAX_COUNT_TOKEN_BY_USER}`}
-            }
+            //     let countUpdateMonth = 0
+            //     tokenInfoData.dateLastUpdate.forEach(t=>{
+            //         const currentDate = new Date(Date.now());
+            //         currentDate.setMonth(currentDate.getMonth() - 1);
+            //         if(currentDate<t)
+            //             countUpdateMonth++
+            //     })
+            //     if(countUpdateMonth>= process.env.MAX_COUNT_REPLACE_TOKEN)
+            //         return {warning:true, message:`Количество авторазий превысело максимально возможное: ${ process.env.MAX_COUNT_TOKEN_BY_USER}`}
+            //     else
+            //         return {warning:true, user:user.id, tokenInfo:{countUpdateMonth, lastCountUpdates:process.env.MAX_COUNT_TOKEN_BY_USER - countUpdateMonth, tokens:tokenInfoData.tokens,}, message:`Количество авторазий превысело максимально возможное: ${ process.env.MAX_COUNT_TOKEN_BY_USER}`}
+            // }
 
-            const lastId = tokenInfoData.tokens.length>0?tokenInfoData.tokens[tokenInfoData.tokens.length-1].id:0
-            tokenInfoData.tokens.push(new Token(lastId+1, token, userInfo))
+            // const lastId = tokenInfoData.tokens.length>0?tokenInfoData.tokens[tokenInfoData.tokens.length-1].id:0
+            // tokenInfoData.tokens.push(new Token(lastId+1, token, userInfo))
             await tokenInfoData.save()
             return {warning:false, token}
         }
